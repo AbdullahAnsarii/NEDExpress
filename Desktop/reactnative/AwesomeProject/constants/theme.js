542033b5e213649b53f65e1d368eb0c8ec193ae6
Abdullah Ansari <aa9272137@gmail.com>
2021-02-25 22:39:40 +0500

    maincategories flat list created

--- Desktop/reactnative/AwesomeProject/constants/theme.js
+++ Desktop/reactnative/AwesomeProject/constants/theme.js
@@ -3,7 +3,7 @@ const { width, height } = Dimensions.get("window");
 
 export const COLORS = {
     // base colors
-    primary: "#FC6D3F", // orange
+    primary: "#008080", // orange
     secondary: "#CDCDD2",   // gray
 
     // colors
