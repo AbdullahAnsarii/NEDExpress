7a8254b09ee6061769b0d416feef7e9c952a2aee
Abdullah Ansari <aa9272137@gmail.com>
2021-03-28 23:50:58 +0500

    firebase connected

--- Desktop/reactnative/AwesomeProject/App.js
+++ Desktop/reactnative/AwesomeProject/App.js
@@ -1,32 +1,44 @@
 import React from 'react';
-
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native'
-
-import {Home, Restaurant, OrderDelivery} from './screens'
-import LoginScreen from "./screens/LoginScreen"
-import SignupScreen from "./screens/SignupScreen"
-import Tabs from './navigation/tabs'
-
-const Stack = createStackNavigator();
+import Providers from './navigation';
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-                //initialRouteName={'Home'}
-            >  
-                <Stack.Screen name= "Onboarding" component={LoginScreen}></Stack.Screen>
-                <Stack.Screen name= "Signup" component={SignupScreen}></Stack.Screen>
-                <Stack.Screen name="Home" component={Tabs} />
-                <Stack.Screen name="Restaurant" component={Restaurant} />
-                <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return <Providers />;
 }
 
 export default App;
+
+
+// import React from 'react';
+
+// import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from '@react-navigation/native'
+
+// import {Home, Restaurant, OrderDelivery} from './screens'
+// import OnboardingScreen from "./screens/OnboardingScreen"
+// import LoginScreen from "./screens/LoginScreen"
+// import SignupScreen from "./screens/SignupScreen"
+// import Tabs from './navigation/tabs'
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//     return (
+//         <NavigationContainer>
+//             <Stack.Navigator
+//                 screenOptions={{
+//                     headerShown: false
+//                 }}
+//                 //initialRouteName={'Home'}
+//             >
+//                 <Stack.Screen name="Onboarding" component={OnboardingScreen}/>
+//                 <Stack.Screen name= "Login" component={LoginScreen}></Stack.Screen>
+//                 <Stack.Screen name= "Signup" component={SignupScreen}></Stack.Screen>
+//                 <Stack.Screen name="Home" component={Tabs} />
+//                 <Stack.Screen name="Restaurant" component={Restaurant} />
+//                 <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     )
+// }
+
+// export default App;
