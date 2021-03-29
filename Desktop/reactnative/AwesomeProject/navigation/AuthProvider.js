9b3bd93ce8452c5e005c8e267d89dd4328aa1506
Abdullah Ansari <aa9272137@gmail.com>
2021-03-29 19:23:15 +0500

    Google Sign In Working!!!!

--- Desktop/reactnative/AwesomeProject/navigation/AuthProvider.js
+++ Desktop/reactnative/AwesomeProject/navigation/AuthProvider.js
@@ -1,7 +1,7 @@
 import React, {createContext, useState} from 'react';
 import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
-// import { GoogleSignin } from '@react-native-community/google-signin';
+//import firestore from '@react-native-firebase/firestore';
+import { GoogleSignin } from '@react-native-community/google-signin';
 // import { LoginManager, AccessToken } from 'react-native-fbsdk';
 
 export const AuthContext = createContext();
@@ -18,31 +18,67 @@ export const AuthProvider = ({children}) => {
             await auth().signInWithEmailAndPassword(email, password);
           } catch (e) {
             console.log(e);
-          }
-        },
-        register: async (email, password) => {
+          }},
+          googleLogin: async () => {
             try {
-              await auth().createUserWithEmailAndPassword(email, password)
-              .then(() => {
-                //Once the user creation has happened successfully, we can add the currentUser into firestore
-                //with the appropriate details.
-                firestore().collection('users').doc(auth().currentUser.uid)
-                .set({
-                    fname: '',
-                    lname: '',
-                    email: email,
-                    createdAt: firestore.Timestamp.fromDate(new Date()),
-                    userImg: null,
-                })
-                //ensure we catch any errors at this stage to advise us if something does go wrong
-                .catch(error => {
-                    console.log('Something went wrong with added user to firestore: ', error);
-                })
-              })
+              // Get the users ID token
+              const { idToken } = await GoogleSignin.signIn();
+  
+              // Create a Google credential with the token
+              const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+              // Sign-in the user with the credential
+              await auth().signInWithCredential(googleCredential)
+              // Use it only when user Sign's up, 
+              // so create different social signup function
+              // .then(() => {
+              //   //Once the user creation has happened successfully, we can add the currentUser into firestore
+              //   //with the appropriate details.
+              //   // console.log('current User', auth().currentUser);
+              //   firestore().collection('users').doc(auth().currentUser.uid)
+              //   .set({
+              //       fname: '',
+              //       lname: '',
+              //       email: auth().currentUser.email,
+              //       createdAt: firestore.Timestamp.fromDate(new Date()),
+              //       userImg: null,
+              //   })
+              //   //ensure we catch any errors at this stage to advise us if something does go wrong
+              //   .catch(error => {
+              //       console.log('Something went wrong with added user to firestore: ', error);
+              //   })
+              // })
               //we need to catch the whole sign up process if it fails too.
               .catch(error => {
                   console.log('Something went wrong with sign up: ', error);
               });
+            } catch(error) {
+              console.log({error});
+            }
+          },
+        register: async (email, password) => {
+            try {
+              await auth().createUserWithEmailAndPassword(email, password)
+              //.then(() => {
+                //Once the user creation has happened successfully, we can add the currentUser into firestore
+                //with the appropriate details.
+                //firestore().collection('users').doc(auth().currentUser.uid)
+              //   .set({
+              //       fname: '',
+              //       lname: '',
+              //       email: email,
+              //       createdAt: firestore.Timestamp.fromDate(new Date()),
+              //       userImg: null,
+              //   })
+              //   //ensure we catch any errors at this stage to advise us if something does go wrong
+              //   .catch(error => {
+              //       console.log('Something went wrong with added user to firestore: ', error);
+              //   })
+              // })
+              // //we need to catch the whole sign up process if it fails too.
+              // .catch(error => {
+              //     console.log('Something went wrong with sign up: ', error);
+              // });
             } catch (e) {
               console.log(e);
             }
--- Desktop/reactnative/AwesomeProject/navigation/AuthStack.js
+++ Desktop/reactnative/AwesomeProject/navigation/AuthStack.js
@@ -3,34 +3,34 @@ import {View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import SignupScreen from '../screens/SignupScreen';
 import LoginScreen from '../screens/LoginScreen';
-import OnboardingScreen from '../screens/OnboardingScreen';
+//import OnboardingScreen from '../screens/OnboardingScreen';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-// import AsyncStorage from '@react-native-community/async-storage';
-// import { GoogleSignin } from '@react-native-community/google-signin';
+//import AsyncStorage from '@react-native-community/async-storage';
+import { GoogleSignin } from '@react-native-community/google-signin';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
-  //const [isFirstLaunch, setIsFirstLaunch] = useState(null);
-  let routeName;
+//const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+let routeName;
 
-  // useEffect(() => {
-  //   AsyncStorage.getItem('alreadyLaunched').then((value) => {
-  //     if (value == null) {
-  //       AsyncStorage.setItem('alreadyLaunched', 'true'); // No need to wait for `setItem` to finish, although you might want to handle errors
-  //       setIsFirstLaunch(true);
-  //     } else {
-  //       setIsFirstLaunch(false);
-  //     }
-  //   }); // Add some error handling, also you can simply do setIsFirstLaunch(null)
+  useEffect(() => {
+    // AsyncStorage.getItem('alreadyLaunched').then((value) => {
+    //   if (value == null) {
+    //     AsyncStorage.setItem('alreadyLaunched', 'true'); // No need to wait for `setItem` to finish, although you might want to handle errors
+    //     setIsFirstLaunch(true);
+    //   } else {
+    //     setIsFirstLaunch(false);
+    //   }
+    // }); // Add some error handling, also you can simply do setIsFirstLaunch(null)
   
-    // GoogleSignin.configure({
-    //   webClientId: 'YOUR_APP_WEB_CLIENT_ID',
-    // });
+    GoogleSignin.configure({
+      webClientId: '292778080288-61c89v8nm1puk41rdp82hvqbls766qfh.apps.googleusercontent.com',
+    });
   
-  // }, []);
+  }, []);
 
   // if (isFirstLaunch === null) {
   //   return null; // This is the 'tricky' part: The query to AsyncStorage is not finished, but we have to present something to the user. Null will just render nothing, so you can also put a placeholder of some sort, but effectively the interval between the first mount and AsyncStorage retrieving your data won't be noticeable to the user. But if you want to display anything then you can use a LOADER here
